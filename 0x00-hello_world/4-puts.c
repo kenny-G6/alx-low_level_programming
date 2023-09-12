2e01fc16f2211f261196e1444983386b9836eee9
kenny-G6 <kehinde694@gmail.com>
2023-09-11 18:23:26 -0700

    fifth

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with puts function
+ * Return:0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
